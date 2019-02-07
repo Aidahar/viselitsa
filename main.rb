ec37f90a1e105cf5f9241106b4861b63b069d436
Aidahar <zagirik@gmail.com>
2019-02-08 00:04:10 +0300

    fix image show

--- main.rb
+++ main.rb
@@ -3,10 +3,12 @@ require_relative "lib/result_printer"
 require_relative "lib/word_reader"
 require 'unicode'
 
+current_path = __dir__
+
 puts "Игра виселица. Версия 3. (c) goodprogrammer.ru\n\n"
 sleep 1
 
-printer = ResultPrinter.new
+printer = ResultPrinter.new(current_path)
 word_reader = WordReader.new
 words_file_name = File.dirname(__FILE__) + "/data/words.txt"
 game = Game.new(word_reader.read_from_file(words_file_name))
