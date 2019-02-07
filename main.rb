05d113bed08a9d7eb8f6162b8a49a4dc0b5218c2
Aidahar <zagirik@gmail.com>
2019-02-07 23:13:29 +0300

    edit reading file

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ sleep 1
 printer = ResultPrinter.new
 word_reader = WordReader.new
 words_file_name = File.dirname(__FILE__) + "/data/words.txt"
-game = Game.new(word_reader.read_from_args)
+game = Game.new(word_reader.read_from_file(words_file_name))
 
 while game.status == 0
   printer.print_status(game)
