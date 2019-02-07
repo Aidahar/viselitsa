d9db3e536882dfd3020973fefd2f2d6b0f34dc43
Aidahar <zagirik@gmail.com>
2019-02-07 22:39:36 +0300

    editing naim file

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
-require_relative "game"
-require_relative "result_printer"
-require_relative "word_reader"
+require_relative "lib/game"
+require_relative "lib/result_printer"
+require_relative "lib/word_reader"
 require 'unicode'
 
 puts "Игра виселица. Версия 3. (c) goodprogrammer.ru\n\n"
